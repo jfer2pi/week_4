2edfe9d02a1bcb6f484ffa3e0f65bd3cec84e1c4
Fernando Rodriguez Alvarado <fernandorodrig@gmail.com>
2017-08-13 22:58:18 -0500

    Updates

--- rankall.R
+++ rankall.R
@@ -3,6 +3,7 @@ rankall <- function(outcome, num = "best") {
   valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
   
   states <- unique(outcomes[, 7])
+  states <- states[order(states)]
   
   #Error finder for the condition, if a outcome is not within the three desired conditions, an error is thrown
   if(length(valid_outcomes[valid_outcomes == outcome]) == 0) {
@@ -20,21 +21,37 @@ rankall <- function(outcome, num = "best") {
   
   condition <- outcomes[, c(2, 7, selector)]
   condition[, 3] <- as.numeric(condition[, 3])
-  condition_complete <- condition[complete.cases(condition) ,]
+  condition_complete <- condition[complete.cases(condition),]
   
-  for(s in states) {
-      
+  ordering <- order(condition_complete[, 2], condition_complete[, 3], condition_complete[, 1])
+  
+  condition_complete_ordered <- condition_complete[ordering ,]
+  
+  #print(tail(condition_complete_ordered))
+  
+  #print(subset(condition_complete_ordered, State == states[11]))
+  
+  if(num == "best") {
+      num <- 1
   }
-
+  else {
+      num
+  }
   
-  #ordering <- order(condition_complete[, 2], condition_complete[, 3], condition_complete[, 1])
-  #condition_complete_ordered <- condition_complete[ordering ,]
+  out <- data.frame()
   
-  #state_subsets <- data.frame()
-  
-  #for(i in c(1:54)) {
-  #  state_subsets[[i]] <- rbind(subset(condition_complete_ordered, condition_complete_ordered[, 2] == states[i]))
-  #}
-  #state_subsets
-  
+  for(state in 1:length(states))
+  {
+      tester <- subset(condition_complete_ordered, State == states[state])[, c(1,2)]
+      print(c(nrow(tester), states[state]))
+      
+      if(nrow(tester) < num) {
+         out <- rbind(out, c(NA, states[state]))
+         print(out)
+        }
+      else {
+        out <- rbind(out, tester)
+         }
+  }
+  out
 }
